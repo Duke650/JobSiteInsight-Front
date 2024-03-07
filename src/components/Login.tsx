77a95f049377be9a8d8df4e72a13eafbbd4988c1
Michael McEwingE <michamcewing@gmail.com>
2024-03-07 14:36:49 -0800

    error handling for signup and log in

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,33 +1,95 @@
-import "../static/styles/login.css"
+import { useState } from "react";
+import "../static/styles/login.css";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [loginUser, setLoginUser] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://127.0.0.1:5000/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(loginUser),
+      });
+
+      if (!response.ok) {
+        setError("Incorrct Username or Password");
+        return;
+      }
+      setShowMessage(() => true);
+      setTimeout(async () => {
+        navigate("/");
+      }, 1500);
+    } catch (error) {
+      setError("Error logging in");
+    }
+  };
+
   return (
-    <div className="my-login-form-container">
-      <form>
-        <div className="form-group">
-          <label htmlFor="exampleInputEmail1">Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            placeholder="Enter email"
-          />
+    <>
+      {showMessage && (
+        <div
+          className="alert alert-success alert-dismissible fade show"
+          role="alert"
+        >
+          <strong>Welcome Back!</strong>
+          <button
+            type="button"
+            className="close"
+            data-dismiss="alert"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">×</span>
+          </button>
         </div>
-        <div className="form-group">
-          <label htmlFor="exampleInputPassword1">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-            placeholder="Password"
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </div>
+      )}
+      <div className="my-login-form-container">
+        <form>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+              onChange={(e) =>
+                setLoginUser({ ...loginUser, email: e.target.value })
+              }
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword2">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword2"
+              placeholder="Password"
+              onChange={(e) =>
+                setLoginUser({ ...loginUser, password: e.target.value })
+              }
+            />
+          </div>
+          {error && <p>{error}</p>}
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={(e) => handleLogin(e)}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,100 +1,167 @@
-import "../static/styles/signup.css"
+import { useNavigate } from "react-router-dom";
+import "../static/styles/signup.css";
 import { useState } from "react";
 
 const Signup = () => {
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [error, setError] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
 
-const [firstName, setFirstName] = useState("")
-const [lastName, setLastName] = useState("")
-const [username, setUsername] = useState("")
-const [email, setEmail] = useState("")
-const [phoneNumber, setPhoneNumber] = useState(0)
-const [password, setPassword] = useState("")
-const [confirmPassword, setConfirmPassword] = useState("")
+  const navigate = useNavigate();
 
-// const signupUser = async () => {
-//     const request = 
-// }
+  const handleSignup = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (user.password === user.confirmPassword) {
+      try {
+        const response = await fetch("http://127.0.0.1:5000/signup", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            first_name: user.firstName,
+            last_name: user.lastName,
+            username: user.username,
+            email: user.email,
+            phone_number: user.phoneNumber,
+            password: user.password,
+          }),
+        });
+        console.log(response);
+
+        if (!response.ok) {
+          const errorData = await response.json();
+          setError(errorData.message);
+          return;
+        } else {
+          setShowMessage(() => true);
+          setTimeout(() => {
+            navigate("/login");
+          }, 1500);
+        }
+      } catch (error) {
+        setError("Error signing up.. Please try again");
+      }
+    } else {
+      setError("Passwords do not match");
+    }
+  };
 
   return (
     <>
+      {showMessage && (
+        <div
+          className="alert alert-success alert-dismissible fade show"
+          role="alert"
+        >
+          <strong>Thank you for signing up!</strong>
+          <button
+            type="button"
+            className="close"
+            data-dismiss="alert"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+      )}
       <div className="my-signup-form-container">
-        <form>
+        <form onSubmit={(e) => handleSignup(e)}>
           <div className="form-row">
             <div className="form-group col-md-6">
-              <label htmlFor="inputEmail4">First Name</label>
+              <label htmlFor="inputName4">First Name</label>
               <input
                 type="text"
                 className="form-control"
-                id="inputEmail4"
+                id="inputName4"
                 placeholder="First Name"
-                onChange={e => setFirstName(e.target.value)}
+                onChange={(e) =>
+                  setUser({ ...user, firstName: e.target.value })
+                }
+                required
               />
             </div>
-            
+
             <div className="form-group col-md-6">
-              <label htmlFor="inputPassword4">Last Name</label>
+              <label htmlFor="inputName1">Last Name</label>
               <input
                 type="text"
                 className="form-control"
-                id="inputPassword4"
+                id="inputName1"
                 placeholder="Last Name"
-                onChange={e => setLastName(e.target.value)}
+                onChange={(e) => setUser({ ...user, lastName: e.target.value })}
+                required
               />
             </div>
             <div className="form-group col-md-6">
-              <label htmlFor="inputEmail4">Username</label>
+              <label htmlFor="inputEmail5">Username</label>
               <input
                 type="text"
                 className="form-control"
                 id="inputEmail5"
                 placeholder="Username"
-                onChange={e => setUsername(e.target.value)}
+                onChange={(e) => setUser({ ...user, username: e.target.value })}
+                required
               />
             </div>
-            
+
             <div className="form-group col-md-6">
-              <label htmlFor="inputPassword4">Email</label>
+              <label htmlFor="inputEmail8">Email</label>
               <input
                 type="email"
                 className="form-control"
-                id="inputPassword4"
+                id="inputEmail8"
                 placeholder="Email"
-                onChange={e => setEmail(e.target.value)}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+                required
               />
             </div>
-            
           </div>
           <div className="form-group">
             <label htmlFor="inputAddress">Phone Number</label>
             <input
               type="tel"
-              placeholder="123-45-6789" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" required
+              placeholder="123-454-6789"
+              pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
+              required
               className="form-control"
               id="inputAddress"
-              onChange={e => setPhoneNumber(Number(e.target.value))}
+              onChange={(e) =>
+                setUser({ ...user, phoneNumber: e.target.value })
+              }
             />
           </div>
           <div className="form-group">
-            <label htmlFor="inputAddress">Password</label>
+            <label htmlFor="inputPassword4">Password</label>
             <input
               type="password"
               placeholder="Password"
               className="form-control"
               id="inputPassword4"
-              onChange={e => setPassword(e.target.value)}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              required
             />
           </div>
           <div className="form-group">
-            <label htmlFor="inputAddress">Confirm Password</label>
+            <label htmlFor="inputPassword5">Confirm Password</label>
             <input
               type="password"
               placeholder="Confirm Password"
               className="form-control"
-              id="inputPassword4"
-              onChange={e => setConfirmPassword(e.target.value)}
+              id="inputPassword5"
+              onChange={(e) =>
+                setUser({ ...user, confirmPassword: e.target.value })
+              }
+              required
             />
           </div>
-
+          {error && <p>{error}</p>}
           <button type="submit" className="btn btn-primary">
             Sign up
           </button>
@@ -104,6 +171,4 @@ const [confirmPassword, setConfirmPassword] = useState("")
   );
 };
 
-{
-}
 export default Signup;
